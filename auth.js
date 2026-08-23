d4430febaa52ef19cdd2f340786a879898e2f9ee
harafy-app <yasserelyan123@gmail.com>
2026-08-23 14:16:29 +0300

    Delete auth.js

--- auth.js
+++
@@ -1,2 +0,0 @@
-import {users} from '../data/demo-data.js';
-export function authenticate(identifier,password){return users.find(u=>(u.id===identifier||u.email.toLowerCase()===identifier.toLowerCase())&&u.password===password)||null}
